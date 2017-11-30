e6d46823f52831df7ba934fc0aa1d3e84674e654
David Arellano <david@Davids-MacBook-Pro.local>
2017-11-29 20:04:32 -0800

    added redux to project

---
+++ app/containers/App.js
@@ -0,0 +1,72 @@
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
+import search from '../components/helpers/search.js';
+import Home from '../components/Home.js';
+import Login from '../components/Login.js';
+import NotFound from '../components/NotFound.js';
+import Profile from '../components/Profile.js';
+import getPreferences from '../components/helpers/getPreferences.js';
+
+import { getUser } from '../actions/appActions.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('/auth')
+      .then((authStatus) => {
+        this.setState({
+          loggedIn: authStatus.data.loggedIn,
+          user: authStatus.data.user,
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <Home search={search} getPreferences={getPreferences} />}
+          />
+          <Route path="/login" component={Login} />
+          <Route
+            path="/profile"
+            render={() =>
+              (this.state.loggedIn ? <Profile user={this.state.user} /> : <Redirect to="/" />)
+            }
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.loggedIn,
+  user: state.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getUser: (loggedIn, user) => {
+    dispatch(getUser(loggedIn, user));
+  },
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
